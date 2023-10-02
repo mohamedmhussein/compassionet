60dea4100237a04ddb258f081f3f4d85fe8e2efa
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-10-02 06:29:57 -0400

    Adds Kindness model

--- server/models.py
+++ server/models.py
@@ -7,13 +7,14 @@ from config import db
 # Models go here!
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, nullable=False)
     _password_hash = db.Column(db.String, nullable=False)
 
     #relationships
-    
+
 
     @hybrid_property
     def password_hash(self):
@@ -29,3 +30,11 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
+
+class Kindness(db.Model, SerializerMixin):
+    __tablename__ = 'kindnesses'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    recipient = db.Column(db.String)
