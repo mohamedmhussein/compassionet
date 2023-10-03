94ee4783d50dbcef0f001d28ac6bf73b622482e4
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-10-02 23:33:45 -0400

    Adds relationships to models

--- server/models.py
+++ server/models.py
@@ -14,7 +14,15 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String, nullable=False)
 
     #relationships
+    kindness = db.relationship('Kindness', backref='performer', lazy=True)
+    comments = db.relationship('Comment', backref='commenter', lazy=True)
+    liked_kindness = db.relationship('Kindness', secondary='like', back_populates='liked_by')
 
+    # reltionships
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    liked_by = db.relationship('User', secondary='like', back_populates='liked_kindness')
+    comments = db.relationship('Comment', backref='on_kindness', lazy=True)
 
     @hybrid_property
     def password_hash(self):
@@ -35,14 +43,36 @@ class Kindness(db.Model, SerializerMixin):
     __tablename__ = 'kindnesses'
 
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
     description = db.Column(db.String, nullable=False)
     date = db.Column(db.String, nullable=False)
-    recipient = db.Column(db.String)
+
+    #relationships
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'), nullable=False)
+
+    liked_by = db.relationship('User', secondary='like', back_populates='liked_kindness')
+    comments = db.relationship('Comment', backref='on_kindness', lazy=True)
+
+# Like Model (Many-to-Many Relationship)
+like = db.Table('like',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('kindness_id', db.Integer, db.ForeignKey('kindness.id'), primary_key=True)
+)
 
 class Comment(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
 
-class Reward(db.Model, SerializerMixin):
+    # Link Comment to User and Kindness
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    kindness_id = db.Column(db.Integer, db.ForeignKey('kindness.id'), nullable=False)
+
+class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    description = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(200), unique=True, nullable=False)
+
+# user-category model
+user_category = db.Table('user_category',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
